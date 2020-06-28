863e43dde290603cd2041ba38c28b4e0b65eec64
Edgar <edgar.a.rivera08@gmail.com>
2020-06-28 12:09:38 -0400

    User info is being displayed to on user show page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import { Nav, Items, NewItem, User, NewUser } from './Components';
 import { Route, Switch } from 'react-router-dom';
 
 
+const USER_URL = 'http://localhost:3000/api/v1/users'
+
 class App extends React.Component {
   state = {
-
+    user: {}
   }
+
+  componentDidMount() {
+    const id = 1
+    fetch(`${USER_URL}/${id}`)
+      .then(res => res.json())
+      .then(userData => {
+        this.setState({
+          user: userData
+        })
+      })
+  }
+
 
   render() {
     return (
@@ -17,7 +31,7 @@ class App extends React.Component {
         <Switch>
 
           <Route path='/signup' component={NewUser} />
-          <Route path='/users/:id' component={User} />
+          <Route path='/users/:id' render={(routerProps) => <User {...routerProps} user={this.state.user}/>} />
 
           <Route path='/newItem' component={NewItem} />
           <Route path='/' component={Items} />
--- src/Components/User.js
+++ src/Components/User.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-class User extends React.Component{
 
-    render(){
+class User extends React.Component {
+
+    render() {
+        const {id, name, username} = this.props.user
+        console.log(this.props)
         return (
             <div>
                 User Show Page
+                <div>
+                    id: {id}
+                    name {name}
+                    Username: {username}
+                </div>
             </div>
         )
     }
